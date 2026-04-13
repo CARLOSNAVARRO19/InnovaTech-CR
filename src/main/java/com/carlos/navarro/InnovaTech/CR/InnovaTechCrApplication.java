1efc268b71cc3c44e1fba5c82aee5bc7cdf48bc0
carlosnavarro19 <carlosnavarror19@gmail.com>
2026-04-12 19:27:40 -0600

    initial commit

---
+++ src/main/java/com/carlos/navarro/InnovaTech/CR/InnovaTechCrApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.navarro.InnovaTech.CR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovaTechCrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovaTechCrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlos/navarro/InnovaTech/CR/InnovaTechCrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.navarro.InnovaTech.CR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovaTechCrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
